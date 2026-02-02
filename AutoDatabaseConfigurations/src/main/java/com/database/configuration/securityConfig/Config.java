6cfd45267c42114086614b0cf0072d358df7c8d8
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2026-02-02 14:12:38 +0530

    commit

--- AutoDatabaseConfigurations/src/main/java/com/database/configuration/securityConfig/Config.java
+++ AutoDatabaseConfigurations/src/main/java/com/database/configuration/securityConfig/Config.java
@@ -1,26 +1,27 @@
 package com.database.configuration.securityConfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.database.configuration.filter.JwtAuthFilter;
 import com.database.configuration.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class Config {
-
+	@Autowired
+    JwtAuthFilter jwtAuthFilter;
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -29,8 +30,9 @@ public class Config {
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/Authenticate").permitAll()
                     .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());
+            );
+           //.httpBasic(Customizer.withDefaults()); // added basic authentication filter , if removed tehn it will remove basic authentication filter
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
