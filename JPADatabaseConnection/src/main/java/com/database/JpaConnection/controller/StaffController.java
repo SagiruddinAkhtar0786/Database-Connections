756c45916615cffeb918307635c3545434c73973
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2026-02-22 13:31:43 +0530

    commit

--- JPADatabaseConnection/src/main/java/com/database/JpaConnection/controller/StaffController.java
+++ JPADatabaseConnection/src/main/java/com/database/JpaConnection/controller/StaffController.java
@@ -3,6 +3,8 @@ package com.database.JpaConnection.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,15 +15,28 @@ import com.database.JpaConnection.entity.Staff;
 
 @RestController
 public class StaffController {
-	
+
 	@Autowired
 	private StaffService staffService;
-	
+
 	@GetMapping("/getStaff")
-	public List<Staff> getStaffDetails() {
+	public List<Staff> getStaff() {
 		return staffService.getAllStaff();
 	}
-	
+
+	@GetMapping("/getStaffDetail")
+	public ResponseEntity<List<Staff>> getStaffDetails() {
+		return new ResponseEntity<>(staffService.getAllStaff(), HttpStatus.OK);
+
+		/*
+		 * List<Staff> staffList = staffService.getAllStaff();
+		 * 
+		 * if (staffList.isEmpty()) { return ResponseEntity.noContent().build(); }
+		 * 
+		 * return ResponseEntity.ok(staffList);
+		 */
+	}
+
 	@PostMapping("/createStaff")
 	public Staff createStaffDetails(@RequestBody Staff stafDetal) {
 		return (Staff) staffService.createStaffDetails(stafDetal);
